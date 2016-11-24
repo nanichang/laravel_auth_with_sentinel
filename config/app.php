f129f39590547d17cb6a192c5b93c24fe1c92b0b
nanichang <nanipaul68@gmail.com>
2016-11-24 21:53:00 +0100

    published sentinel

--- config/app.php
+++ config/app.php
@@ -178,6 +178,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // ACL service providers
+        Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
+
+
     ],
 
     /*
@@ -225,6 +229,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Sentinel Alliases
+        'Activation' => Cartalyst\Sentinel\Laravel\Facades\Activation::class,
+        'Reminder'   => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
+        'Sentinel'   => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
 
     ],
 
