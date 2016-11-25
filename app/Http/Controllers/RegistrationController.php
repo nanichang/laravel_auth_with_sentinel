50c6ea9215beb8b021d5b7678f5bcbd6b3ea004e
nanichang <nanipaul68@gmail.com>
2016-11-25 18:21:22 +0100

    added post method the RegistrationController

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Sentinel;
+// use Request;
 
 class RegistrationController extends Controller
 {
     public function register() {
     	return view('auth.register');
+    }
+
+    public function store(Request $request) {
+    	// $this->validate($request, [
+     //        'name' => 'required',
+     //        'code' => 'required',
+     //    ]);
+
+    	$user = Sentinel::registerAndActivate($request->all());
+		dd($user);
     }
 
 
